aa2ef2c87e02fd1bb3dab2f258f1c0f719a9ad7c
Stefan Genov <michaelpalacce@gmail.com>
2020-08-09 01:16:15 +0300

    [master] Even more scuffed metrics

--- src/main/metrics.js
+++ src/main/metrics.js
@@ -5,8 +5,17 @@ const io				= require( '@pm2/io' );
 const app				= require( 'event_request' )();
 const DataServerMap		= require( 'event_request/server/components/caching/data_server_map' );
 
-const dataServer		= new DataServerMap( { ttl: -1, useBigMap: true, persistInterval: 100, persist: true, persistPath: path.join( os.tmpdir(), 'uniqueVisitors' ) } );
+const dataServer		= new DataServerMap( { ttl: -1, useBigMap: true, persistInterval: 5, persist: true, persistPath: path.join( os.tmpdir(), 'uniqueVisitors' ) } );
 const uniqueVisitors	= io.counter( { name: 'Realtime unique visitors', id: 'app/realtime/unique-visitors' } );
+const totalVisitorsKey	= 'totalVisitors';
+
+async function initMetrics()
+{
+	if ( await dataServer.get( totalVisitorsKey ) === null )
+		await dataServer.set( totalVisitorsKey, 0 );
+
+	uniqueVisitors.inc( await dataServer.get( totalVisitorsKey ) );
+}
 
 app.add( async ( event ) => {
 	const md5Sum	= crypto.createHash( 'md5' );
@@ -16,8 +25,11 @@ app.add( async ( event ) => {
 	if ( await dataServer.get( result ) === null )
 	{
 		await dataServer.set( result, '0' );
+		await dataServer.increment( totalVisitorsKey, 1 );
 		uniqueVisitors.inc();
 	}
 
 	event.next();
 });
+
+initMetrics();
