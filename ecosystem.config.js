1310df772b6adb7ac79a0b89089ff945148a67fb
Stefan Genov <stefantigro@gmail.com>
2020-08-12 11:50:57 +0300

    [master] Progress

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
-module.exports = {
-  apps : [
-    {
-      script: 'index.js',
-      watch: false,
-      max_memory_restart: '200M',
-      exp_backoff_restart_delay: 100
-    }
-  ]
+module.exports	=
+{
+	apps :
+	[
+		{
+			script: 'index.js',
+			watch: false,
+			max_memory_restart: '200M',
+			exp_backoff_restart_delay: 100
+		}
+	]
 };
